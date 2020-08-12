aee0c2c9635ab889bc66cbb65195a354ee7259d0
calt08 <cesarlopez2808@gmail.com>
2020-08-11 22:02:40 -0400

    some changes

--- client.py
+++ client.py
@@ -4,11 +4,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 8080))
 
 msg_to_send = input("insert your message: ")
+
 # Send msg
 s.send(bytes(msg_to_send, "utf-8"))
 
-input("Press enter")
+input("Press enter!")  # Just to make it wait
 
 # Recieve msg
 msg = s.recv(1024)  # buffer
 print(msg.decode("utf-8"))
+s.close()
--- echoServer.py
+++ echoServer.py
@@ -2,14 +2,16 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 8080))
+print("Listening on port 8080")
+print()
 s.listen(5)
 
 while True:
     clientsocket, adress = s.accept()
     print(f"Connected to {adress}")
-    # clientsocket.send(bytes("Welcome!", "utf-8"))
     msg = clientsocket.recv(1024)  # buffer
     msgData = msg.decode("utf-8")
+
     print("Recieved:" + msgData)
     print("Sending text...")
     clientsocket.send(msg)
